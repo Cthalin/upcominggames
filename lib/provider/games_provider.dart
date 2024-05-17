478f24efdf72db16a24edf338b7ed0c13b665d59
Cthalin <erik.slowikowski@gmail.com>
2024-05-17 21:09:26 +0200

    feat: add game details caching

--- lib/provider/games_provider.dart
+++ lib/provider/games_provider.dart
@@ -11,34 +11,53 @@ class GamesNotifier extends StateNotifier<GamesState> {
   GamesNotifier() : super(GamesState.initial());
 
   Future<void> loadGames({String? startDate, String? endDate}) async {
-    state = state.copyWith(isLoading: true);
-    FirebaseFunctions functions = FirebaseFunctions.instance;
-    final callable = functions.httpsCallableFromUrl(
-      "https://europe-west1-upcominggamesapp.cloudfunctions.net/fetchGames",
-    );
-    await callable().then(
-      (result) => {
-        state = state.copyWith(
-          games: (result.data as List)
-              .map((game) => MinimalGame.fromJson(game))
-              .toList(),
-          isLoading: false,
-        ),
-      },
-    );
+    try {
+      state = state.copyWith(isLoading: true);
+      FirebaseFunctions functions = FirebaseFunctions.instance;
+      final callable = functions.httpsCallableFromUrl(
+        "https://europe-west1-upcominggamesapp.cloudfunctions.net/fetchGames",
+      );
+      await callable().then(
+        (result) => {
+          state = state.copyWith(
+            games: (result.data as List)
+                .map((game) => MinimalGame.fromJson(game))
+                .toList(),
+            isLoading: false,
+          ),
+        },
+      );
+    } catch (e) {
+      state = state.copyWith(isLoading: false, error: e.toString());
+    }
   }
 
   Future<void> getGameById(int id) async {
-    FirebaseFunctions functions = FirebaseFunctions.instance;
-    final callable = functions.httpsCallableFromUrl(
-      "https://europe-west1-upcominggamesapp.cloudfunctions.net/fetchGameDetails",
-    );
-    await callable.call(<String, dynamic>{'gameId': id}).then(
-      (value) => state = state.copyWith(
-        selectedGame: Game.fromJson(value.data),
+    final exists = state.loadedGames.any((element) => element.id == id);
+    if (exists) {
+      state = state.copyWith(
+        selectedGame:
+            state.loadedGames.firstWhere((element) => element.id == id),
         isLoading: false,
-      ),
-    );
+      );
+      return;
+    }
+    try {
+      state = state.copyWith(isLoading: true);
+      FirebaseFunctions functions = FirebaseFunctions.instance;
+      final callable = functions.httpsCallableFromUrl(
+        "https://europe-west1-upcominggamesapp.cloudfunctions.net/fetchGameDetails",
+      );
+      await callable.call(<String, dynamic>{'gameId': id}).then(
+        (value) => state = state.copyWith(
+          selectedGame: Game.fromJson(value.data),
+          isLoading: false,
+          loadedGames: [...state.loadedGames, Game.fromJson(value.data)],
+        ),
+      );
+    } catch (e) {
+      state = state.copyWith(isLoading: false, error: e.toString());
+    }
   }
 
   void loadNextMonth() {
@@ -55,12 +74,14 @@ class GamesState {
   final Game? selectedGame;
   final bool isLoading;
   final String? error;
+  final List<Game> loadedGames;
 
   GamesState({
     required this.games,
     this.selectedGame,
     required this.isLoading,
     this.error,
+    required this.loadedGames,
   });
 
   factory GamesState.initial() {
@@ -68,6 +89,7 @@ class GamesState {
       games: [],
       isLoading: false,
       error: null,
+      loadedGames: [],
     );
   }
 
@@ -76,12 +98,14 @@ class GamesState {
     Game? selectedGame,
     bool? isLoading,
     String? error,
+    List<Game>? loadedGames,
   }) {
     return GamesState(
       games: games ?? this.games,
       selectedGame: selectedGame ?? this.selectedGame,
       isLoading: isLoading ?? this.isLoading,
       error: error ?? this.error,
+      loadedGames: loadedGames ?? this.loadedGames,
     );
   }
 }
