3e714a7a28179a4af529ca3f5bc293d7bec8527d
Cthalin <erik.slowikowski@gmail.com>
2024-05-17 22:38:19 +0200

    feat: add bottom nav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
-import 'package:upcoming_games/provider/date_provider.dart';
 import 'package:upcoming_games/theme.dart';
 import 'package:upcoming_games/widgets/games_list.dart';
 
@@ -34,7 +32,12 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
                   Spacing.m,
                   Spacing.s,
                 ),
-                child: Image.asset('assets/images/header.png', height: 30),
+                child: Image.asset(
+                  'assets/images/header.png', height: 30,
+                  color: Colors.teal,
+                  // Tint color
+                  colorBlendMode: BlendMode.srcIn,
+                ),
               ),
               const Expanded(child: Text(' Games')),
             ],
@@ -47,47 +50,28 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               GamesList(),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                      child: const Text(
-                        "Previous month",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () =>
-                          ref.read(dateProvider.notifier).previousMonth(),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      DateFormat("MMM")
-                          .format(
-                            DateTime.parse(ref.watch(dateProvider).currentDate),
-                          )
-                          .toUpperCase(),
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      child: const Text(
-                        "Next month",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      onPressed: () =>
-                          ref.read(dateProvider.notifier).nextMonth(),
-                    ),
-                  ),
-                ],
-              ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Wishlist',
+          ),
+        ],
+        currentIndex: 0, // TODO change
+        selectedItemColor: Colors.teal,
+        onTap: null, // TODO implement
       ),
     );
   }
