c929c1e757eaad5e3f77d343466d3e4f61f45672
Cthalin <erik.slowikowski@gmail.com>
2024-05-21 15:33:09 +0200

    feat: add wishlist icon on gameCard

--- lib/widgets/game_card.dart
+++ lib/widgets/game_card.dart
@@ -1,18 +1,21 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:upcoming_games/models/minimal_game.dart';
+import 'package:upcoming_games/provider/wishlist_provider.dart';
 
 import '../app_router.dart';
 
-class GameCard extends StatelessWidget {
+class GameCard extends ConsumerWidget {
   final MinimalGame game;
 
   const GameCard(this.game, {super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final router = AutoRouter.of(context);
+    final isWishlisted = ref.watch(wishlistProvider).contains(game.id);
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
@@ -50,6 +53,23 @@ class GameCard extends StatelessWidget {
                 placeholder: (context, url) =>
                     const CircularProgressIndicator(),
                 errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              child: IconButton(
+                icon:
+                    Icon(isWishlisted ? Icons.favorite : Icons.favorite_border),
+                color: Colors.white,
+                onPressed: () {
+                  isWishlisted
+                      ? ref
+                          .read(wishlistProvider.notifier)
+                          .removeFromWishlist(game.id)
+                      : ref
+                          .read(wishlistProvider.notifier)
+                          .addToWishlist(game.id);
+                },
               ),
             ),
             Align(
